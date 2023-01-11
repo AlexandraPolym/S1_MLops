3f2c8fcdf80bf6d03d49903e3241180c03a9a0ba
melmel6 <s212558@student.dtu.dk>
2023-01-11 20:33:31 +0100

    delete test_model

--- test_model.py
+++
@@ -1,9 +0,0 @@
-from tests import _PATH_DATA
-from model import MyAwesomeModel
-import pytest
-
-
-def test_error_on_wrong_shape():
-#    with pytest.raises(ValueError, match='Expected input to a 4D tensor'):
-#       model(torch.randn(1,2,3))
-    assert 0
